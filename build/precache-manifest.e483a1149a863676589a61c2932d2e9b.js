c934c0b810cd5fbb8f2723eb516ec93a00e32677
Mubarak <mikail.mubarak1@gmail.com>
2020-08-20 11:38:42 +0200

    Production build to heroku

---
+++ build/precache-manifest.e483a1149a863676589a61c2932d2e9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f384dfce83a523e16453c62d821f96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2c210a125d2358b1822",
+    "url": "/static/css/main.2bebb4a5.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c210a125d2358b1822",
+    "url": "/static/js/main.afe6ca58.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
